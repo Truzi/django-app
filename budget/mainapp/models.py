249e60430aad21dc6b186c54daa28e6aeefd5f4a
truzi <mj.gerasimiuk@gmail.com>
2023-05-13 00:05:08 +0200

    modified models to auto add date

--- budget/mainapp/models.py
+++ budget/mainapp/models.py
@@ -1,16 +1,26 @@
 from django.db import models
+from datetime import date
 
 class Category(models.Model):
   name = models.CharField(max_length=30)
 
+  def __str__(self) -> str:
+    return self.name
+
 class Income(models.Model):
   amount = models.FloatField()
-  date = models.DateField()
+  date = models.DateField(auto_now_add = True)
   description = models.CharField(max_length=100)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self) -> str:
+    return f'{self.amount}$ for {self.description}'
   
 class Expense(models.Model):
   amount = models.FloatField()
-  date = models.DateField()
+  date = models.DateField(auto_now_add = True)
   description = models.CharField(max_length=100)
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self) -> str:
+    return f'{self.amount}$ for {self.description}'
