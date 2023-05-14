88f5d4a5962a42580c511389fe58dbf77c0a99c6
Marcin Gerasimiuk <marcin.gerasimiuk@reckitt.com>
2023-05-14 13:03:06 +0200

    added 'remove_item' to urls.py

--- budget/mainapp/urls.py
+++ budget/mainapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add_income', views.add_income, name="add_income"),
     path('expenses', views.expenses, name="expenses"),
     path('add_expense', views.add_expense, name="add_expense"),
-    path('summary', views.summary, name='summary')
+    path('summary', views.summary, name='summary'),
+    path('remove_item', views.remove_item, name="remove_item")
 ]
