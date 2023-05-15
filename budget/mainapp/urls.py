fb290e3d1dd937c2e9141c50102a4b71b5a40520
truzi <mj.gerasimiuk@gmail.com>
2023-05-16 01:02:01 +0200

    organized views

--- budget/mainapp/urls.py
+++ budget/mainapp/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('income', views.income, name="income"),
-    path('add_income', views.add_income, name="add_income"),
     path('expenses', views.expenses, name="expenses"),
-    path('add_expense', views.add_expense, name="add_expense"),
     path('summary', views.summary, name='summary'),
+    
+    path('add_income', views.add_income, name="add_income"),
+    path('add_expense', views.add_expense, name="add_expense"),
     path('remove_item', views.remove_item, name="remove_item")
 ]
